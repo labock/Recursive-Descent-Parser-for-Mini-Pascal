ac1a8414d69a3a07a1147c896732db85ce0f6d5e
labock <lukebock6@gmail.com>
2015-11-08 10:54:44 -0600

    chel

--- src/chel.java
+++ src/chel.java
@@ -1,6 +1,10 @@
+import javax.xml.bind.SchemaOutputResolver;
+import java.util.Scanner;
+
 /**
  * Created by Luke's PC on 11/8/2015.
  */
 
 public class chel {
+    Scanner conSoleAh = new Scanner(System.in);
 }
